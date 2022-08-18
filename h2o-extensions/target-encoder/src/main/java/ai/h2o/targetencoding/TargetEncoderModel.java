aff666720548263770052714d42ab9dfae71fde8
Sebastien Poirier <sebastien@h2o.ai>
2022-08-18 21:53:24 +0200

    core Pipeline API

--- h2o-extensions/target-encoder/src/main/java/ai/h2o/targetencoding/TargetEncoderModel.java
+++ h2o-extensions/target-encoder/src/main/java/ai/h2o/targetencoding/TargetEncoderModel.java
@@ -634,7 +634,7 @@ public class TargetEncoderModel extends Model<TargetEncoderModel, TargetEncoderM
       Frame workingFrame = fr;
       int teColumnIdx = fr.find(columnToEncode);
       int foldColIdx;
-      if (_outOfFold== NO_FOLD) {
+      if (_outOfFold == NO_FOLD) {
         foldColIdx = fr.find(_foldColumn);
       } else {
         workingFrame = new Frame(fr);
@@ -656,7 +656,7 @@ public class TargetEncoderModel extends Model<TargetEncoderModel, TargetEncoderM
               maxFoldValue
       );
       Scope.track(joinedFrame);
-      if (_outOfFold!= NO_FOLD) {
+      if (_outOfFold != NO_FOLD) {
         joinedFrame.remove(foldColIdx);
       }
 
